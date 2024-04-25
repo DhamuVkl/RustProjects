2643cd39c4ab18bca8af9d9d11188ae3c800b0b6
Dharagesh <sdhamuvkl@gmail.com>
2024-04-25 21:55:29 +0530

    #

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     add(50, 82);
     let result = add_num(500, 200);
     println!("{}", result);
+
+    let ans = add_1(50, 99);
+    println!("{}", ans);
+
 }
 
 fn name() {
@@ -16,5 +20,14 @@ fn add(x: u32, y: u32) {
 
 fn add_num(x: u64, y: u64) -> u64 {
     // function returns value need to specifi return data type.
-    x + y // to return value avoid ;
+    x + y   // to return value avoid ; (or) use->   return x + y;
+}
+
+fn add_1(x: i64, y: i64) -> i64 {
+    let result = x + y;
+    if result > 10 {
+        return result - 100;
+    }
+    return result;
+
 }
