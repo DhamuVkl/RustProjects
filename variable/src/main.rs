b4be62196eae08a88f54757fb64357a0855d94ba
Dharagesh <sdhamuvkl@gmail.com>
2024-04-25 12:16:21 +0530

    #

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let  x = 4;
+    let x = 4;
     println!("x is: {}", x);
     {
         let x = 2;
         println!("x is: {} \n", x);
     }
-     let x = x + 1;
+    let x = x + 1;
 
     println!("x is: {} \n", x);
 }
