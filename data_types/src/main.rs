d9c407e451b58e43989d78f215e78d18e190b40f
Dharagesh <sdhamuvkl@gmail.com>
2024-04-25 12:51:32 +0530

    #

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -4,13 +4,16 @@ fn main() {
     let bol = true;
     let letter = 'e';
     let letter1 = "s";
-    let tuple = (8, true, 's' , "we");
+    let tuple = (8, true, 's', "we");
+    let mut tuple_a = (23, false, 'D', "df");
+    println!("{:?}", tuple_a);
+    tuple_a.0 = 24;
+    println!("{:?}", tuple_a);
     let x: f64 = 10.9;
 
-    println!("{}",bol);
-    println!("{}",letter);
-    println!("{}",letter1);
-    println!("{:?}",tuple);
+    println!("{}", bol);
+    println!("{}", letter);
+    println!("{}", letter1);
+    println!("{:?}", tuple);
     println!("x is {}", x);
 }
- 
