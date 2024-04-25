c5c7b4190d6dd2ccc7d78af6bdadbf835dbc36ed
Dharagesh <sdhamuvkl@gmail.com>
2024-04-25 21:45:54 +0530

    #

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
     println!("Hello, world!");
     name();
-    add(50,82);
-    
+    add(50, 82);
+    let result = add_num(500, 200);
+    println!("{}", result);
 }
 
 fn name() {
@@ -11,4 +12,9 @@ fn name() {
 
 fn add(x: u32, y: u32) {
     println!("sum is:{}", x + y);
+}
+
+fn add_num(x: u64, y: u64) -> u64 {
+    // function returns value need to specifi return data type.
+    x + y // to return value avoid ;
 }
