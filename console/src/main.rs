40ee48ad02042069f52dfb0f6f6a1ad02ad93d7d
Dharagesh <sdhamuvkl@gmail.com>
2024-04-25 13:38:25 +0530

    #

--- console/src/main.rs
+++ console/src/main.rs
@@ -1,11 +1,9 @@
 use std::io;
 
-
 fn main() {
     println!("Hello, world!");
     let mut input = String::new();
     let msg = "Failed to Read";
     io::stdin().read_line(&mut input).expect(msg);
     println!("{}", input);
-    
 }
