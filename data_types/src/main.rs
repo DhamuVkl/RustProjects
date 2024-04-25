5cbcd1c23c5bec451ef19e88eade1422503be7f6
Dharagesh <sdhamuvkl@gmail.com>
2024-04-25 13:03:39 +0530

    #

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,17 +1,26 @@
 fn main() {
     println!("Hello, world!");
 
-    let bol = true;
-    let letter = 'e';
-    let letter1 = "s";
-    let tuple = (8, true, 's', "we");
-    let mut tuple_a = (23, false, 'D', "df");
-    println!("{:?}", tuple_a);
-    tuple_a.0 = 24;
-    println!("{:?}", tuple_a);
     let x: f64 = 10.9;
 
+    let bol = true;
+
+    let letter = 'e';
+
+    let letter1 = "s";
+
+    let tuple = (8, true, 's', "we");
+
+    let mut tuple_a = (23, false, 'D', "df");   // you can't expand the tuple even in mut once it was intilaized.
+
+    println!("{:?}", tuple_a);
+
+    tuple_a.0 = 24;     // also you can't change the data type (int-> char) in tuple once it was intilaized.
+
+    println!("{:?}", tuple_a);
+
     let mut arr = [0, 1, 2, 3, 4];
+
     println!("{}", arr[3]);
     println!("{:?}", arr);
     arr[3] = 8;
