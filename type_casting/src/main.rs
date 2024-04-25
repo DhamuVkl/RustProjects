73b344a5306b32d97c32184781411ec928139a49
Dharagesh <sdhamuvkl@gmail.com>
2024-04-25 14:32:09 +0530

    #

--- type_casting/src/main.rs
+++ type_casting/src/main.rs
@@ -1,3 +1,6 @@
+use  std::io;
+
+
 fn main() {
     // let x: u8 = 255; // u8 unsigned int 8bit => 0 to 255
     // let y: i8 = 10; // i8 signed int -128 to 127
@@ -6,7 +9,7 @@ fn main() {
 
     // let x: i64 = 120;
     // let y: i32 = 10;
-    // let z = x / y as i64;    // we changed y to i64 type now no error will appear.
+    // let z = x / y as i64;     // we changed y to i64 type now no error will appear.
     // println!("{}", z);
 
 
@@ -19,6 +22,11 @@ fn main() {
     // let y = 10;
     // let z = x/y;
     // println!("{}", z); // o/p 12 not as 12.5 to get 12.5 put both x,y in float.
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let check_input: i64 = input.trim().parse().unwrap();
+    println!("{}", check_input + 2);
 
 
  }
