db8c0c163352b246829291afb69f8f993b889093
Dharagesh <sdhamuvkl@gmail.com>
2024-04-25 14:15:10 +0530

    #

--- type_casting/src/main.rs
+++ type_casting/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
-}
+    // let x: u8 = 255; // u8 unsigned int 8bit => 0 to 255
+    // let y: i8 = 10; // i8 signed int -128 to 127
+    //let z = x + y;        // you cant add two diff types eventhough it has same value & bits.
+    // println!("{}", z);
+
+    // let x: i64 = 120;
+    // let y: i32 = 10;
+    // let z = x / y as i64;    // we changed y to i64 type now no error will appear.
+    // println!("{}", z);
+
+
+    // let x: u8 = 255;
+    // let y: u8 = 1;
+    // let z = x + y; // 256 overflow occurs because z as u8 even if you change to u16 it doesn't allow you!
+    // println!("{}", z);
+
+    // let x = 125;
+    // let y = 10;
+    // let z = x/y;
+    // println!("{}", z); // o/p 12 not as 12.5 to get 12.5 put both x,y in float.
+
+
+ }
